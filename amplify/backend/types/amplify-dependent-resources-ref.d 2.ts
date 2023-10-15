41a853ced979996da595bd11f4b955cdbe500f88
KyeongHun Kim <kimkhuna1@gmail.com>
2023-10-15 18:29:13 +0900

    .gitigonore 추가

--- amplify/backend/types/amplify-dependent-resources-ref.d 2.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "UniNuri": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
